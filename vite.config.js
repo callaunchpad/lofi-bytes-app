3692529b2765280b74cbb3bfbfc078240125ac26
Alena Chao <alenachao@berkeley.edu>
2023-03-21 18:09:37 -0700

    imported svrg to support svg

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import * as path from 'path';
+import svgr from 'vite-plugin-svgr'; 
+//https://dev.to/cassidoo/importing-svg-files-as-react-components-with-vite-l3n
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(),], 
   build: {
     sourcemap: false,
   },
