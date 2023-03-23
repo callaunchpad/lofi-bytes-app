a4c1bd6ba1ccde120f1d75f8cdf7b9f7059081b1
Alena Chao <alenachao@berkeley.edu>
2023-03-23 16:01:14 -0700

    added transformer generated music

--- src/components/AudioPlayer/transformertracks.js
+++ src/components/AudioPlayer/transformertracks.js
@@ -1,21 +1,37 @@
 import imgSrc from "@/media/lofi3.gif";
 import imgSrc2 from "@/media/lofi1.gif";
-import lstm_rain from "./assets/lstm_final.mp3";
-import lstm from "./assets/download.wav";
+import classical1 from "./assets/model_alicia_400_classical-27.mp3";
+import mario1 from "./assets/model_alicia_400_generated-mario.mp3";
+import classical2 from "./assets/model_zane_classical.mp3";
+import mario2 from "./assets/model_zane_mario.mp3";
 
 // All of these artists are at https://pixabay.com/music/search/mood/laid%20back/
 export default [
   {
     title: "Music Transformer 1",
-    artist: "classical dataset",
-    audioSrc: lstm,
+    artist: "alicia - classical",
+    audioSrc: classical1,
     image: imgSrc,
     color: "#00aeb0"
   },
   {
     title: "Music Transformer 2",
-    artist: "a diff dataset",
-    audioSrc: lstm_rain,
+    artist: "alicia - mario",
+    audioSrc: mario1,
+    image: imgSrc2,
+    color: "#ffb77a"
+  },
+  {
+    title: "Music Transformer 3",
+    artist: "zane - classical",
+    audioSrc: classical2,
+    image: imgSrc,
+    color: "#00aeb0"
+  },
+  {
+    title: "Music Transformer 4",
+    artist: "zane - mario",
+    audioSrc: mario2,
     image: imgSrc2,
     color: "#ffb77a"
   },
